b4acf4e77bc82740eabe1f0df099d24c0eaaae19
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2020-11-28 22:55:13 +0530

    Create SimpleApi.go

---
+++ SimpleApi.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//
+//
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Welcome to the HomePage!")
+// 	fmt.Println("Endpoint Hit: homePage")
+// }
+
+// func handleRequests() {
+// 	http.HandleFunc("/", homePage)
+// 	log.Fatal(http.ListenAndServe(":10000", nil))
+// }
+
+// func main() {
+// 	handleRequests()
+// }
+
+//(2)// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+var Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	// add our articles route and map it to our
+	// returnAllArticles function like so
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
